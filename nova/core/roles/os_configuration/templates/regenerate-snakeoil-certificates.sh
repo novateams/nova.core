6aec3599ab24876d55a44358c9a48e4fbfa19765
Allar Viik <allar@clarifiedsecurity.com>
2023-11-09 14:21:33 +0200

    Regenerating Snakeoil certificates only if `make-ssl-cert` command exists

---
+++ nova/core/roles/os_configuration/templates/regenerate-snakeoil-certificates.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+# Regenerating snakeoil certificates if make-ssl-cert command is available
+# Needs ssl-cert packages installed
+if [[ -x "$(command -v make-ssl-cert)" ]]; then
+
+    make-ssl-cert generate-default-snakeoil --force-overwrite
+
+else
+
+    echo "make-ssl-cert command not found, skipping"
+
+fi
+
