878f359ae3daae2a000ed7b2fe84a944beb3d99e
Allar Viik <allar@clarifiedsecurity.com>
2023-11-20 16:53:51 +0200

    Updated the PowerShell optimization script in `template_os_configuration` role

--- nova/core/roles/template_os_configuration/files/Optimize-PowerShell.ps1
+++ nova/core/roles/template_os_configuration/files/Optimize-PowerShell.ps1
@@ -1,22 +1,49 @@
 $ErrorActionPreference = 'Stop'
 
 # https://docs.ansible.com/ansible/latest/os_guide/windows_performance.html#optimize-powershell-performance-to-reduce-ansible-task-overhead
-function Optimize-PowershellAssemblies {
-	# NGEN powershell assembly, improves startup time of powershell by 10x
-	$old_path = $env:path
-	try {
-	  $env:path = [Runtime.InteropServices.RuntimeEnvironment]::GetRuntimeDirectory()
-	  [AppDomain]::CurrentDomain.GetAssemblies() | % {
-		if (! $_.location) {continue}
-		$Name = Split-Path $_.location -leaf
-		if ($Name.startswith("Microsoft.PowerShell.")) {
-		  Write-Progress -Activity "Native Image Installation" -Status "$name"
-		  ngen install $_.location | % {"`t$_"}
-		}
-	  }
-	} finally {
-	  $env:path = $old_path
-	}
-  }
+function Optimize-Assemblies {
+    param (
+        [string]$assemblyFilter = "Microsoft.PowerShell.",
+        [string]$activity = "Native Image Installation"
+    )
 
-Optimize-PowershellAssemblies
+    try {
+        # Get the path to the ngen executable dynamically
+        $ngenPath = [System.IO.Path]::Combine([Runtime.InteropServices.RuntimeEnvironment]::GetRuntimeDirectory(), "ngen.exe")
+
+        # Check if ngen.exe exists
+        if (-Not (Test-Path $ngenPath)) {
+            Write-Host "Ngen.exe not found at $ngenPath. Make sure .NET Framework is installed."
+            return
+        }
+
+        # Get a list of loaded assemblies
+        $assemblies = [AppDomain]::CurrentDomain.GetAssemblies()
+
+        # Filter assemblies based on the provided filter
+        $filteredAssemblies = $assemblies | Where-Object { $_.FullName -ilike "$assemblyFilter*" }
+
+        if ($filteredAssemblies.Count -eq 0) {
+            Write-Host "No matching assemblies found for optimization."
+            return
+        }
+
+        foreach ($assembly in $filteredAssemblies) {
+            # Get the name of the assembly
+            $name = [System.IO.Path]::GetFileName($assembly.Location)
+
+            # Display progress
+            Write-Progress -Activity $activity -Status "Optimizing $name"
+
+            # Use Ngen to install the assembly
+            Start-Process -FilePath $ngenPath -ArgumentList "install `"$($assembly.Location)`"" -Wait -WindowStyle Hidden
+        }
+
+        Write-Host "Optimization complete."
+    } catch {
+        Write-Host "An error occurred: $_"
+    }
+}
+
+# Optimize PowerShell assemblies:
+Optimize-Assemblies -assemblyFilter "Microsoft.PowerShell."
