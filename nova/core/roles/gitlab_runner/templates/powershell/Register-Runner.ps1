82d7917d9eaebebcb1f2e3a260c9f7302167baad
Lauri Leitma <lauri.leitma@clarifiedsecurity.com>
2024-07-04 17:09:03 +0300

    Added `gitlab_runner` role

---
+++ nova/core/roles/gitlab_runner/templates/powershell/Register-Runner.ps1
@@ -0,0 +1,14 @@
+$ErrorActionPreference = "Stop"
+
+$RunnerName = "{{ runner.name }}"
+$GitLabURI = "{{gitlab_runner_gitlab_url }}"
+$RunnerPath = "{{ gitlab_runner_windows_config_folder }}"
+$RunnerToken = "{{ runner.auth_token }}"
+$Template = "{{ gitlab_runner_windows_config_folder }}\{{ runner.name }}\gitlab-runner-config.template.toml"
+
+Set-Location $RunnerPath # Set location is required for the runner to use the config file in $RunnerPath
+.\gitlab-runner.exe register --non-interactive `
+--name $RunnerName `
+--url $GitLabURI `
+--token $RunnerToken `
+--template-config $Template
