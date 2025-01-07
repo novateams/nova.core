8c2e0a840bab43abb94c403e325127e91dd23b4e
Allar Viik <allar@clarifiedsecurity.com>
2025-01-07 23:39:18 +0200

    Removed hardcoded Windows interface name from `configure_networking` role

--- nova/core/roles/configure_networking/templates/Configure-Network.ps1
+++ nova/core/roles/configure_networking/templates/Configure-Network.ps1
@@ -8,28 +8,29 @@ Get-NetRoute | Where-Object InterfaceAlias -NotLike Loopback* | Remove-NetRoute 
 # Looping over Providentia interfaces
 {% for interface in interfaces %}
 
-$Interface = "Ethernet{{ loop.index -1 }}"
+{% set interface_loop = loop.index -1 %}
+$Interfaces = @(Get-NetAdapter | Select-Object -ExpandProperty Name | Sort-Object)
 
 # Looping over IP addresses
 {% for ip_address in interface.addresses %}
 
 # Disabling DHCP and Router Discovery for the interface
-Set-NetIPInterface -InterfaceAlias $Interface -Dhcp Disabled -RouterDiscovery Disabled
+Set-NetIPInterface -InterfaceAlias $Interfaces[{{ interface_loop }}] -Dhcp Disabled -RouterDiscovery Disabled
 
 {% if (ip_address.mode == 'ipv4_static') and (ip_address.gateway != none) %}
 
 # Excluding IPv4 gateway if it already exists
 if ($null -eq (Get-NetIPConfiguration | Select-Object -ExpandProperty IPv4DefaultGateway)) {
 
-    New-NetIPAddress -AddressFamily IPv4 -IPAddress {{ ip_address.address | ansible.utils.ipaddr('address') }} -InterfaceAlias $Interface -PrefixLength {{ ip_address.address | ansible.utils.ipaddr('prefix') }} -DefaultGateway {{ ip_address.gateway }}
+    New-NetIPAddress -AddressFamily IPv4 -IPAddress {{ ip_address.address | ansible.utils.ipaddr('address') }} -InterfaceAlias $Interfaces[{{ interface_loop }}] -PrefixLength {{ ip_address.address | ansible.utils.ipaddr('prefix') }} -DefaultGateway {{ ip_address.gateway }}
 
 } else {
 
-    New-NetIPAddress -AddressFamily IPv4 -IPAddress {{ ip_address.address | ansible.utils.ipaddr('address') }} -InterfaceAlias $Interface -PrefixLength {{ ip_address.address | ansible.utils.ipaddr('prefix') }}
+    New-NetIPAddress -AddressFamily IPv4 -IPAddress {{ ip_address.address | ansible.utils.ipaddr('address') }} -InterfaceAlias $Interfaces[{{ interface_loop }}] -PrefixLength {{ ip_address.address | ansible.utils.ipaddr('prefix') }}
 }
 
 {% elif (ip_address.mode == 'ipv4_static') and (ip_address.gateway == none) %}
-New-NetIPAddress -AddressFamily IPv4 -IPAddress {{ ip_address.address | ansible.utils.ipaddr('address') }} -InterfaceAlias $Interface -PrefixLength {{ ip_address.address | ansible.utils.ipaddr('prefix') }}
+New-NetIPAddress -AddressFamily IPv4 -IPAddress {{ ip_address.address | ansible.utils.ipaddr('address') }} -InterfaceAlias $Interfaces[{{ interface_loop }}] -PrefixLength {{ ip_address.address | ansible.utils.ipaddr('prefix') }}
 {% endif %}
 
 {% if (ip_address.mode == 'ipv6_static') and (ip_address.gateway != none) %}
@@ -37,21 +38,21 @@ New-NetIPAddress -AddressFamily IPv4 -IPAddress {{ ip_address.address | ansible.
 # Excluding IPv6 gateway if it already exists
 if ($null -eq (Get-NetIPConfiguration | Select-Object -ExpandProperty IPv6DefaultGateway)) {
 
-    New-NetIPAddress -AddressFamily IPv6 -IPAddress {{ ip_address.address | ansible.utils.ipaddr('address') }} -InterfaceAlias $Interface -PrefixLength {{ ip_address.address | ansible.utils.ipaddr('prefix') }} -DefaultGateway {{ ip_address.gateway }}
+    New-NetIPAddress -AddressFamily IPv6 -IPAddress {{ ip_address.address | ansible.utils.ipaddr('address') }} -InterfaceAlias $Interfaces[{{ interface_loop }}] -PrefixLength {{ ip_address.address | ansible.utils.ipaddr('prefix') }} -DefaultGateway {{ ip_address.gateway }}
 
 } else {
 
-    New-NetIPAddress -AddressFamily IPv6 -IPAddress {{ ip_address.address | ansible.utils.ipaddr('address') }} -InterfaceAlias $Interface -PrefixLength {{ ip_address.address | ansible.utils.ipaddr('prefix') }}
+    New-NetIPAddress -AddressFamily IPv6 -IPAddress {{ ip_address.address | ansible.utils.ipaddr('address') }} -InterfaceAlias $Interfaces[{{ interface_loop }}] -PrefixLength {{ ip_address.address | ansible.utils.ipaddr('prefix') }}
 }
 
 {% elif (ip_address.mode == 'ipv6_static') and (ip_address.gateway == none) %}
-New-NetIPAddress -AddressFamily IPv6 -IPAddress {{ ip_address.address | ansible.utils.ipaddr('address') }} -InterfaceAlias $Interface -PrefixLength {{ ip_address.address | ansible.utils.ipaddr('prefix') }} -SkipAsSource $true
+New-NetIPAddress -AddressFamily IPv6 -IPAddress {{ ip_address.address | ansible.utils.ipaddr('address') }} -InterfaceAlias $Interfaces[{{ interface_loop }}] -PrefixLength {{ ip_address.address | ansible.utils.ipaddr('prefix') }} -SkipAsSource $true
 {% endif %}
 
 {% endfor %}
 
 {% if dns_server_combined != [] %}
-Set-DnsClientServerAddress -InterfaceAlias $Interface -ServerAddresses {{ dns_server_combined | join(', ') }}
+Set-DnsClientServerAddress -InterfaceAlias $Interfaces[{{ interface_loop }}] -ServerAddresses {{ dns_server_combined | join(', ') }}
 {% endif %}
 
 {% endfor %}
