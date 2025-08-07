9c82bc987a2e846df80b84a51a85bfc3a9f6e271
Allar Viik <allar@clarifiedsecurity.com>
2025-08-07 18:37:56 +0300

    Fixed Proxmox VM interface name lookup in `configure_networking` role

--- nova/core/roles/configure_networking/templates/proxmox.sh
+++ nova/core/roles/configure_networking/templates/proxmox.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+#!/bin/bash
+
+set -e # exit when any command fails
 
 # Logging script output
 exec > /tmp/network.log 2>&1
-
-set -e # exit when any command fails
 
 # Timeout duration in seconds
 TIMEOUT=180 # 5 minutes
@@ -21,7 +21,7 @@ until systemctl is-active --quiet pve-cluster; do
     sleep 3
 done
 
-MGMT_INTERFACE_NAME={{ configure_networking_proxmox_mgmt_interface_name | default("$(ip -o link show | awk -F': ' '{print $2}' | grep -v '^lo$' | head -n 1)") }}
+MGMT_INTERFACE_NAME={{ configure_networking_proxmox_mgmt_interface_name | default("$(ip -o link show | awk -F': ' '{print $2}' | grep -v '^lo$' | grep -v '^ens.*' | head -n 1)") }}
 NODE_NAME={{ configure_networking_proxmox_node_name | default("$(hostname)") }}
 
 pvesh set /nodes/$NODE_NAME/network/$MGMT_INTERFACE_NAME --type bridge \
