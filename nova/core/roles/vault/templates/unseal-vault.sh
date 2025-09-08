db11f968cc96fef0ee74fd3e207c400487b60ced
Allar Viik <allar@clarifiedsecurity.com>
2025-09-08 13:02:30 +0300

    Refactored `vault` auto-unseal script to use only docker command and running it once per minute

--- nova/core/roles/vault/templates/unseal-vault.sh
+++ nova/core/roles/vault/templates/unseal-vault.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 
-VAULT_UNSEAL_KEY="{{ vault_unseal_key }}"
+set -e
 
 #----------End of variables, start of script----------#
 
-while ! curl -k {{ vault_configuration_uri }}
+VAULT_UNSEAL_KEY="{{ vault_unseal_key }}"
+CONTAINER_STATUS=$(sudo docker inspect -f {{ '{{.State.Running}}' }} {{ vault_container_name }})
+SEAL_STATUS=$(sudo docker exec vault vault status | grep 'Sealed' | awk '{print $2}')
 
-do
+if [[ "$CONTAINER_STATUS" != "true" ]]; then
 
-  echo "Waiting until container is running"
-  sleep 10
+    echo "Waiting for 30 seconds for the container to start..."
+    sleep 5
 
-done
+fi
 
-if sudo docker exec vault vault status; then
+if [[ "$CONTAINER_STATUS" != "true" ]]; then
 
-    echo "Vault is unsealed."
+    echo "Container is not running. Please start the container first."
+
+elif [[ "$CONTAINER_STATUS" == "true" && "$SEAL_STATUS" == "false" ]]; then
+
+    echo "Vault is already unsealed."
 
 else
 
-    echo "Vault is sealed!"
-    echo "Unsealing..."
-    sudo docker exec vault vault operator unseal $VAULT_UNSEAL_KEY
+    echo "Unsealing Vault..."
+    sudo docker exec vault vault operator unseal "$VAULT_UNSEAL_KEY"
 
 fi
