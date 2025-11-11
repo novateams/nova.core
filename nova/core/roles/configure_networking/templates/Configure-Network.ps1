e7200411222d4bbcd8a2d3aee04293a485fe5136
Allar Viik <allar@clarifiedsecurity.com>
2025-11-11 11:03:47 +0200

    In `configure_networking` role fixed a Windows network configuration race-condtiton when multiple interfaces were used

--- nova/core/roles/configure_networking/templates/Configure-Network.ps1
+++ nova/core/roles/configure_networking/templates/Configure-Network.ps1
@@ -10,62 +10,63 @@ $Interfaces = @(Get-NetAdapter | Where-Object { $_.HardwareInterface -eq $true }
 # Looping over Providentia interfaces
 {% for interface in interfaces %}
 
-    {% set interface_loop = loop.index0 %}
+{% set interface_loop = loop.index0 %}
 
-    # If no IP addresses are defined, enable DHCP and Router Discovery
-    {% if interface.addresses == [] %}
-    Set-NetIPInterface -InterfaceAlias $Interfaces[{{ interface_loop }}] -Dhcp Enabled -RouterDiscovery Enabled
-    Set-DnsClientServerAddress -InterfaceAlias $Interfaces[{{ interface_loop }}] -ResetServerAddresses
-    Restart-NetAdapter -Name $Interfaces[{{ interface_loop }}]
-    {% else %}
-    Set-NetIPInterface -InterfaceAlias $Interfaces[{{ interface_loop }}] `
-    -Dhcp {{ 'Enabled' if interface.addresses | map(attribute='mode') | intersect(['ipv4_dhcp']) else 'Disabled' }} `
-    -RouterDiscovery {{ 'Enabled' if interface.addresses | map(attribute='mode') | intersect(['ipv4_dhcp']) else 'Disabled' }}
-    Set-DnsClientServerAddress -InterfaceAlias $Interfaces[{{ interface_loop }}] -ResetServerAddresses
-    Restart-NetAdapter -Name $Interfaces[{{ interface_loop }}]
+# If no IP addresses are defined, enable DHCP and Router Discovery
+{% if interface.addresses == [] %}
+Set-NetIPInterface -InterfaceAlias $Interfaces[{{ interface_loop }}] -Dhcp Enabled -RouterDiscovery Enabled
+Set-DnsClientServerAddress -InterfaceAlias $Interfaces[{{ interface_loop }}] -ResetServerAddresses
+Restart-NetAdapter -Name $Interfaces[{{ interface_loop }}]
+{% else %}
+Set-NetIPInterface -InterfaceAlias $Interfaces[{{ interface_loop }}] `
+-Dhcp {{ 'Enabled' if interface.addresses | map(attribute='mode') | intersect(['ipv4_dhcp']) else 'Disabled' }} `
+-RouterDiscovery {{ 'Enabled' if interface.addresses | map(attribute='mode') | intersect(['ipv4_dhcp']) else 'Disabled' }}
+Set-DnsClientServerAddress -InterfaceAlias $Interfaces[{{ interface_loop }}] -ResetServerAddresses
 
-        # Looping over IP addresses
-        {% if interface.addresses | map(attribute='mode') | intersect(['ipv4_static', 'ipv6_static']) | list | length > 0 %}
-        {% for ip_address in interface.addresses %}
+# Looping over IP addresses
+{% if interface.addresses | map(attribute='mode') | intersect(['ipv4_static', 'ipv6_static']) | list | length > 0 %}
+{% for ip_address in interface.addresses %}
 
-            {% if (ip_address.mode == 'ipv4_static') and (ip_address.gateway != none) %}
+{% if (ip_address.mode == 'ipv4_static') and (ip_address.gateway != none) %}
 
-            # Excluding IPv4 gateway if it already exists
-            if ($null -eq (Get-NetIPConfiguration | Select-Object -ExpandProperty IPv4DefaultGateway)) {
+# Excluding IPv4 gateway if it already exists
+if ($null -eq (Get-NetIPConfiguration | Select-Object -ExpandProperty IPv4DefaultGateway)) {
 
-                New-NetIPAddress -AddressFamily IPv4 -IPAddress {{ ip_address.address | ansible.utils.ipaddr('address') }} -InterfaceAlias $Interfaces[{{ interface_loop }}] -PrefixLength {{ ip_address.address | ansible.utils.ipaddr('prefix') }} -DefaultGateway {{ ip_address.gateway }}
+    New-NetIPAddress -AddressFamily IPv4 -IPAddress {{ ip_address.address | ansible.utils.ipaddr('address') }} -InterfaceAlias $Interfaces[{{ interface_loop }}] -PrefixLength {{ ip_address.address | ansible.utils.ipaddr('prefix') }} -DefaultGateway {{ ip_address.gateway }}
 
-            } else {
+} else {
 
-                New-NetIPAddress -AddressFamily IPv4 -IPAddress {{ ip_address.address | ansible.utils.ipaddr('address') }} -InterfaceAlias $Interfaces[{{ interface_loop }}] -PrefixLength {{ ip_address.address | ansible.utils.ipaddr('prefix') }}
-            }
+    New-NetIPAddress -AddressFamily IPv4 -IPAddress {{ ip_address.address | ansible.utils.ipaddr('address') }} -InterfaceAlias $Interfaces[{{ interface_loop }}] -PrefixLength {{ ip_address.address | ansible.utils.ipaddr('prefix') }}
+}
 
-            {% elif (ip_address.mode == 'ipv4_static') and (ip_address.gateway == none) %}
-            New-NetIPAddress -AddressFamily IPv4 -IPAddress {{ ip_address.address | ansible.utils.ipaddr('address') }} -InterfaceAlias $Interfaces[{{ interface_loop }}] -PrefixLength {{ ip_address.address | ansible.utils.ipaddr('prefix') }}
-            {% endif %}
+{% elif (ip_address.mode == 'ipv4_static') and (ip_address.gateway == none) %}
+New-NetIPAddress -AddressFamily IPv4 -IPAddress {{ ip_address.address | ansible.utils.ipaddr('address') }} -InterfaceAlias $Interfaces[{{ interface_loop }}] -PrefixLength {{ ip_address.address | ansible.utils.ipaddr('prefix') }}
+{% endif %}
 
-            {% if (ip_address.mode == 'ipv6_static') and (ip_address.gateway != none) %}
+{% if (ip_address.mode == 'ipv6_static') and (ip_address.gateway != none) %}
 
-            # Excluding IPv6 gateway if it already exists
-            if ($null -eq (Get-NetIPConfiguration | Select-Object -ExpandProperty IPv6DefaultGateway)) {
+# Excluding IPv6 gateway if it already exists
+if ($null -eq (Get-NetIPConfiguration | Select-Object -ExpandProperty IPv6DefaultGateway)) {
 
-                New-NetIPAddress -AddressFamily IPv6 -IPAddress {{ ip_address.address | ansible.utils.ipaddr('address') }} -InterfaceAlias $Interfaces[{{ interface_loop }}] -PrefixLength {{ ip_address.address | ansible.utils.ipaddr('prefix') }} -DefaultGateway {{ ip_address.gateway }}
+    New-NetIPAddress -AddressFamily IPv6 -IPAddress {{ ip_address.address | ansible.utils.ipaddr('address') }} -InterfaceAlias $Interfaces[{{ interface_loop }}] -PrefixLength {{ ip_address.address | ansible.utils.ipaddr('prefix') }} -DefaultGateway {{ ip_address.gateway }}
 
-            } else {
+} else {
 
-                New-NetIPAddress -AddressFamily IPv6 -IPAddress {{ ip_address.address | ansible.utils.ipaddr('address') }} -InterfaceAlias $Interfaces[{{ interface_loop }}] -PrefixLength {{ ip_address.address | ansible.utils.ipaddr('prefix') }}
-            }
+    New-NetIPAddress -AddressFamily IPv6 -IPAddress {{ ip_address.address | ansible.utils.ipaddr('address') }} -InterfaceAlias $Interfaces[{{ interface_loop }}] -PrefixLength {{ ip_address.address | ansible.utils.ipaddr('prefix') }}
+}
 
-            {% elif (ip_address.mode == 'ipv6_static') and (ip_address.gateway == none) %}
-            New-NetIPAddress -AddressFamily IPv6 -IPAddress {{ ip_address.address | ansible.utils.ipaddr('address') }} -InterfaceAlias $Interfaces[{{ interface_loop }}] -PrefixLength {{ ip_address.address | ansible.utils.ipaddr('prefix') }} -SkipAsSource $true
-            {% endif %}
+{% elif (ip_address.mode == 'ipv6_static') and (ip_address.gateway == none) %}
+New-NetIPAddress -AddressFamily IPv6 -IPAddress {{ ip_address.address | ansible.utils.ipaddr('address') }} -InterfaceAlias $Interfaces[{{ interface_loop }}] -PrefixLength {{ ip_address.address | ansible.utils.ipaddr('prefix') }} -SkipAsSource $true
+{% endif %}
 
-        {% endfor %}
-        {% endif %}
+{% endfor %}
+{% endif %}
 
-        {% if dns_server_combined != [] %}
-        Set-DnsClientServerAddress -InterfaceAlias $Interfaces[{{ interface_loop }}] -ServerAddresses {{ dns_server_combined | join(', ') }}
-        {% endif %}
+{% if dns_server_combined != [] %}
+Set-DnsClientServerAddress -InterfaceAlias $Interfaces[{{ interface_loop }}] -ServerAddresses {{ dns_server_combined | join(', ') }}
+{% endif %}
 
-    {% endif %}
+{% endif %}
+
+Restart-NetAdapter -Name $Interfaces[{{ interface_loop }}]
 {% endfor %}
