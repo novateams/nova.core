70988a85730703dc1f14620521b1dd8dbc510404
Allar Viik <allar@clarifiedsecurity.com>
2026-05-29 19:53:06 +0300

    Combined multiple Windows commands into one in `os_configuration` for faster deploy

--- nova/core/roles/os_configuration/templates/Configure-Windows.ps1
+++ nova/core/roles/os_configuration/templates/Configure-Windows.ps1
@@ -1,5 +1,22 @@
 $ErrorActionPreference = 'Stop'
 
+# Time
+$TimeZone = "UTC"
+Set-Service W32Time -StartupType Automatic
+Start-Service W32Time
+Set-TimeZone -Id $TimeZone
+
+# Power
+powercfg -change -monitor-timeout-ac 0
+powercfg -change -standby-timeout-ac 0
+powercfg -change -disk-timeout-ac 0
+powercfg -change -hibernate-timeout-ac 0
+powercfg -change -monitor-timeout-dc 0
+powercfg -change -standby-timeout-dc 0
+powercfg -change -disk-timeout-dc 0
+powercfg -change -hibernate-timeout-dc 0
+
+{% if kms_server is ansible.builtin.truthy %}
 $KMSServer = "{{ kms_server }}"
 
 #----------End of variables, Start of Script----------#
@@ -19,3 +36,4 @@ if ($null -ne $OfficeActivator) {
     cscript.exe $OfficeActivator /act
 
 }
+{% endif %}
