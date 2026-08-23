0e64debd7d61f0b56844fd1176d3b6f490c450c1
Allar Viik <allar@clarifiedsecurity.com>
2026-08-23 18:05:37 +0300

    Cleaning up history in all Windows profiles in `cleanup`

--- nova/core/roles/cleanup/templates/Windows-Cleanup.ps1
+++ nova/core/roles/cleanup/templates/Windows-Cleanup.ps1
@@ -7,18 +7,11 @@ $AllUserProfiles = Get-ChildItem C:\Users | Select-Object -ExpandProperty Name
 # Looking up all profiles
 foreach ($ProfileName in $AllUserProfiles) {
 
-    # Cleaning up GT & Admin profiles
-    if ($ProfileName -like "{{ gt_username }}" -or $ProfileName -like "{{ admin_account }}") {
-
         Write-Host "Cleaning up $ProfileName's profile"
-
         if ((Test-Path "C:\Users\$ProfileName\AppData\Roaming\Microsoft\Windows\PowerShell\PSReadLine\*") -eq $true) {Remove-Item "C:\Users\$ProfileName\AppData\Roaming\Microsoft\Windows\PowerShell\PSReadLine\*" -Recurse -Force}
         if ((Test-Path "C:\Users\$ProfileName\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\*") -eq $true) {Remove-Item "C:\Users\$ProfileName\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\*" -Recurse -Force}
         if ((Test-Path "C:\Users\$ProfileName\AppData\Local\AppData\Local\Temp\*") -eq $true) {Remove-Item "C:\Users\$ProfileName\AppData\Local\AppData\Local\Temp\*" -Recurse -Force}
-
     }
-
-}
 
 if ((Test-Path "C:\ProgramData\chocolatey\logs\*") -eq $true) {Remove-Item "C:\ProgramData\chocolatey\logs\*" -Recurse -Force}
 if ((Test-Path "C:\ExchangeSetupLogs") -eq $true) {Remove-Item "C:\ExchangeSetupLogs" -Recurse -Force}
